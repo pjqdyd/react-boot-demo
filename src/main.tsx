14ee0c2888393550eb548eb11f502107a64db8c0
节庆 <pengjiqing@cai-inc.com>
2024-10-24 16:41:13 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { Application, ReactBoot } from '@pjqdyd/react-boot'
+import { Application } from './react-boot'
+import { ReactBootApplication } from '@pjqdyd/react-boot'
 import Router from './router'
 import './index.css'
 
-@Application({ name: 'app', description: '我的应用' })
+@Application()
 //eslint-disable-next-line
-class App implements ReactBoot{
+class App implements ReactBootApplication{
     run () {
         ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
             <React.StrictMode>
---
+++ src/react-boot.ts
@@ -0,0 +1,12 @@
+import { ReactBoot } from '@pjqdyd/react-boot'
+
+
+const {
+    Application,
+    createApp,
+} = ReactBoot({
+    name: 'app',
+    description: '我的app'
+});
+
+export { Application, createApp }
