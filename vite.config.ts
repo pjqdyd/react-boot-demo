c063765ea96373b71bd9b71c0e00b5970db3de11
节庆 <pengjiqing@cai-inc.com>
2024-11-21 19:53:25 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// import { Plugin } from 'vite';
-// react 函数如何获取使用的文件位置使用
-// function filePathPlugin(): Plugin {
-//     return {
-//         name: 'vite-plugin-file-path',
-//         transform(code, id) {
-//             if (id.endsWith('.tsx') || id.endsWith('.jsx')) {
-//                 const filePath = id;
-//                 const newCode = `const __filename = '${filePath}';\n${code}`;
-//                 return newCode;
-//             }
-//         },
-//     };
-// }
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
