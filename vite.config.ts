f6e698815f9f55331a4cdb12338facbf37d8379f
节庆 <pengjiqing@cai-inc.com>
2024-12-24 17:37:16 +0800

    update github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-boot-demo/',
+  base: '/', // /react-boot-demo
   plugins: [
       react({
         babel:{
