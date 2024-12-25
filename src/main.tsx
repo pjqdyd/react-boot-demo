98c7b871cfbc768b69e4ab8dc61aa2a62222bb3d
节庆 <pengjiqing@cai-inc.com>
2024-12-25 10:42:28 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { Application } from './react-boot'
+import { Application, createApp } from './react-boot'
 import { ReactBootApplication } from '@pjqdyd/react-boot'
 import Router from './router'
 import './index.css'
 
 import modules from "@/react-modules.ts";
 
+// 启动类启动应用
 @Application({ modules: modules })
 //eslint-disable-next-line
 class App implements ReactBootApplication {
@@ -19,3 +20,15 @@ class App implements ReactBootApplication {
         )
     }
 }
+
+// createApp 启动应用
+// const app = createApp({
+//     modules: modules,
+//     run: () => {
+//         ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//             <React.StrictMode>
+//                 <RouterProvider router={Router} />
+//             </React.StrictMode>
+//         )
+//     }
+// })
